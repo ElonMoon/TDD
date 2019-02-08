e2ebcd46e27f2b466d66e4ec5ecca68294e1f5f1
Joohyun <elonmoon@gmail.com>
2019-02-08 18:48:04 +0900

    목록 출력을 위한 신규 URL, 뷰, 템플릿 작성

--- app/functional_tests/tests.py
+++ app/functional_tests/tests.py
@@ -14,7 +14,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -41,6 +41,8 @@ class NewVisitorTest(LiveServerTestCase):
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # '1: 공작깃털 사기' 아이템이 추가된다.
         input_box.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
@@ -53,13 +55,34 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
 
-        # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다.
-        # 사이트는 그녀를 위한 특정 URL 을 생성해준다.
-        # 이때 URL 에 대한 설명도 함께 제공된다.
+        # 새로운 사용자인 프란시스가 사이트에 접속한다.
+
+        """
+        새로운 브라우저 세션을 이용해서 에디스의 정보가
+        쿠키를 통해 유입되는 것을 방지한다.
+        """
+        self.browser.quit()
+        self.browser = webdriver.Chrome('/usr/local/var/driver/chromedriver')
+
+        # 프란시스가 홈페이지에 접속한다.
+        # 에디스의 리스트는 보이지 않는다.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text)
+        self.assertNotIn('그물 만들기', page_text)
+
+        # 프란시스가 새로안 작업 아이템을 입력하기 시작한다
+        # 그는 에디스보다 재미가 없다
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('우유 사기')
+        input_box.send_keys(Keys.ENTER)
+
+        # 프란시스가 전용 URL 을 취득한다.
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
         self.fail('Finish the test!')
         # 해당 URL 에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다.
 
         # 만족하고 잠자리에 든다.
-
-        self.browser.quit()
--- app/lists/urls.py
+++ app/lists/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
+    path(r'', views.home_page, name='home'),
+    path(r'lists/the-only-list-in-the-world/', views.view_list, name='view-list'),
 ]
