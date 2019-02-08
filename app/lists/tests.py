57847a0885fe985892b286c48315bd3cd9f3b760
Joohyun <elonmoon@gmail.com>
2019-02-08 17:16:59 +0900

    POST 후에 리디렉션과 템플릿에 모든 작업 아이템 표시

--- app/lists/tests.py
+++ app/lists/tests.py
@@ -28,6 +28,16 @@ class ItemModelTest(TestCase):
 
 class HomePageTest(TestCase):
 
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -45,9 +55,21 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': '신규 작업 아이템'},
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertIn(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
--- app/lists/views.py
+++ app/lists/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Item
 
 
 def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
     context = {
-        'new_item_text': request.POST.get('item_text', '')
+        'items': Item.objects.all()
     }
     return render(request, 'home.html', context)
